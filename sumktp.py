69bebb8eddb8b47de35aa1898d332004439b761b
Marisa Paryasto <cryptocodecg@gmail.com>
2015-07-27 08:54:43 +0700

    Revert "regex"
    This reverts commit 4e581a51378ea1ebea6d391c9506d80b35185cb1.

--- sumktp.py
+++ sumktp.py
@@ -1,12 +1,7 @@
 #! /usr/bin/python
 # sum number of ktp
 
-import re
-
 data = raw_input('masukkan nomor KTP: ')
-# print data
-# print len(data)
-
-numbers = re.compile('\d+')
-baru = numbers.findall(data)
-print baru
+print data
+#d = str(data)
+print len(data)
