f16567200e4d4f6809386a4fb7979bc10392beba
Marisa Paryasto <cryptocodecg@gmail.com>
2015-07-28 18:24:32 +0700

    2D array
    modified a bit

--- gen_array.py
+++ gen_array.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python
 import array
 
-my_array = [1,2,3,4,5]
+my_array = [[1,2],[3,4]]
 
-for i in my_array:
-   print i
+for i in range(len(my_array)):
+   for j in range(len(my_array[i])):
+      print my_array[i][j] 
 
