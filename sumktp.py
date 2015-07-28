61fd86929e50986a510407d0e6a09f685b546f0a
Budi Rahardjo <rahard@gmail.com>
2015-07-28 11:18:47 +0700

    cleaning sumktp.py
    Cleaning the source
    Add conversion to 3 digit sum with leading zero(es)

--- sumktp.py
+++ sumktp.py
@@ -1,18 +1,16 @@
 #! /usr/bin/python
-# sum number of ktp
+# menjumlahkan angka nomor KTP
 
 data = raw_input('masukkan nomor KTP: ')
-# print data
-# print len(data)
-
-#numbers = re.compile('\d+')
-#baru = numbers.findall(data)
-#print baru
 
 sumdata = 0
 for i in data:
    if i.isdigit():
-	print i
+	# print i
    	sumdata +=  int(i)
 
-print 'Sum = ', sumdata
+# print 'Sum = ', sumdata
+# format output with leading zero
+
+strsumdata = "%03d" % sumdata
+print strsumdata
