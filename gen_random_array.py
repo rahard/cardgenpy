11f06b5f42b722ebfab8d8537b75d4f04545c7ea
Marisa Paryasto <cryptocodecg@gmail.com>
2015-07-30 09:52:20 +0700

    inserting sum to card
    inserting sum ktp to the card (first row, first three column)

--- gen_random_array.py
+++ gen_random_array.py
@@ -3,7 +3,8 @@ from array import *
 from random import randint
 
 def sumKTP():
-    data = raw_input('masukkan nomor KTP: ')
+#    data = raw_input('masukkan nomor KTP: ')
+    data = "3204062010620003"
 
     sumdata = 0
     for i in data:
@@ -11,13 +12,14 @@ def sumKTP():
             # print i
             sumdata +=  int(i)
 
-    # print 'Sum = ', sumdata
+    print 'Sum = ', sumdata
     # format output with leading zero
 
     strsumdata = "%03d" % sumdata
-    print strsumdata
+    return strsumdata
+#    print strsumdata
 
-sumKTP()
+str_data = sumKTP()
 
 endrows = 10 
 endcols = 20 
@@ -40,4 +42,25 @@ for i in range(len(my_array)):
 
 #print "array with random numbers"
 #print my_array
+
+print
+print str_data[0],
+print str_data[1],
+print str_data[2]
+
+#replace the first 3 numbers in array with sum of KTP number
+my_array[0][0] = str_data[0]
+my_array[0][1] = str_data[1]
+my_array[0][2] = str_data[2]
+
+print my_array[0][0],
+print my_array[0][1],
+print my_array[0][2]
+
+print
+print "new card number",
+for i in range(len(my_array)):
+   print
+   for j in range(len(my_array[i])):
+       print my_array[i][j],
 
