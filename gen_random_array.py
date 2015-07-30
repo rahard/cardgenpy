eb4b6eed1863ef2adda5dfae3100ded058cc2eb9
Marisa Paryasto <cryptocodecg@gmail.com>
2015-07-30 09:02:11 +0700

    printing random arrays
    in a neat rows and columns

--- gen_random_array.py
+++ gen_random_array.py
@@ -2,7 +2,9 @@
 from array import *
 from random import randint
 
-my_array = [ [ 0 for i in range(6) ] for j in range(6) ]
+endrows = 12 
+endcols = 15 
+my_array = [ [ 0 for i in range(endcols) ] for j in range(endrows) ]
 
 def random_num():
     range_start = 0
@@ -10,13 +12,15 @@ def random_num():
     return randint(range_start, range_end)
 
 #print random_num()
-print "empty array"
-print my_array
+#print "empty array"
+#print my_array
 
 for i in range(len(my_array)):
+   print
    for j in range(len(my_array[i])):
        my_array[i][j] = random_num()
+       print my_array[i][j],
 
-print "array with random numbers"
-print my_array
+#print "array with random numbers"
+#print my_array
 
